0d010d6823a8963ff11069376a67af5457841be9
Dmitry Buggenhout <dmitry.buggenhout@gmail.com>
2022-12-02 00:24:55 +0100

    ui

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,112 @@
-use clap::{Parser};
-use std::io;
+use clap::Parser;
+use chrono;
+use std::{io, thread, time::Duration};
+use tui::{
+    backend::{CrosstermBackend, Backend},
+    widgets::{Widget, Block, Borders},
+    layout::{Layout, Constraint, Direction},
+    Terminal, Frame
+};
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+
+static DAYS: [&str; 7] = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 
 #[derive(Parser)]
 struct Cli {
-    path: std::path::PathBuf
+    path: std::path::PathBuf,
 }
 
-fn write_to_file(path: std::path::PathBuf, data: String) -> Result<(), Box<dyn std::error::Error>>{
+fn write_to_file(path: std::path::PathBuf, data: String) -> Result<(), Box<dyn std::error::Error>> {
     let write = std::fs::write(path, data);
 
     match write {
-        Ok(()) => {},
-        Err(error) => {panic!("Could not make file! ERROR: {}", error);}, 
+        Ok(()) => {}
+        Err(error) => {
+            panic!("Could not make file! ERROR: {}", error);
+        }
     };
     Ok(())
 }
 
+fn ask_user_for_input() -> std::string::String {
+    let mut user_input = String::new();
+    let stdin = io::stdin();
+    stdin
+        .read_line(&mut user_input)
+        .expect("Something went wrong.");
+
+    return user_input;
+}
+
+// fn create_terminal() -> Result<(), io::Error> {
+//     let stdout = io::stdout();
+//     let backend = CrosstermBackend::new(stdout);
+//     let mut terminal = Terminal::new(backend)?;
+//     Ok(())
+// }
+
+fn create_terminal() -> Result<(), io::Error> {
+    // setup terminal
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    terminal.draw(|f| {
+        let size = f.size();
+        let block = Block::default();
+        //     .title("Block")
+        //     .borders(Borders::ALL);
+        ui(f);
+        f.render_widget(block, size);
+    })?;
+
+
+    thread::sleep(Duration::from_millis(5000));
+
+    // restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    Ok(())
+}
+
+fn ui<B: Backend>(f: &mut Frame<B>) {
+    let chunks = Layout::default()
+         .direction(Direction::Vertical)
+         .margin(1)
+         .constraints(
+             [
+                 Constraint::Percentage(80),
+                 Constraint::Percentage(20)
+             ].as_ref()
+         )
+         .split(f.size());
+     let block = Block::default()
+          .title("Calendar")
+          .borders(Borders::ALL);
+     f.render_widget(block, chunks[0]);
+     let block = Block::default()
+          .title("Input")
+          .borders(Borders::ALL);
+     f.render_widget(block, chunks[1]);
+ }
+
 fn main() {
     let args = Cli::parse();
 
-    let mut user_input = String::new();
-    let stdin = io::stdin();
-    stdin.read_line(&mut user_input).expect("Something went wrong.");
-
-
-    write_to_file(args.path, user_input).expect("Could not write to file.");
-
+    // Make placeholder for days
+    let my_vec: Vec<u32> = (1..=31).step_by(1).collect();
+    create_terminal();
 
 }
